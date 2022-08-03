2b4b4cda32dddd699a5fe9f251c5cc2cd08a337a
Maciej Adamski <madamskip1@gmail.com>
2022-08-04 01:46:47 +0200

    Add Hu Moments class

--- include/HuMoments.h
+++ include/HuMoments.h
@@ -1,4 +1,8 @@
 #pragma once
+
+#include <initializer_list>
+#include <vector>
+
 #include "CentralMoments.h"
 
 namespace POBR
@@ -6,21 +10,15 @@ namespace POBR
 	class HuMoments
 	{
 	public:
-		double hu_1{ 0.0 };
-		double hu_2{ 0.0 };
-		double hu_3{ 0.0 };
-		double hu_4{ 0.0 };
-		double hu_5{ 0.0 };
-		double hu_6{ 0.0 };
-		double hu_7{ 0.0 };
-		double hu_8{ 0.0 };
-		double hu_9{ 0.0 };
-		double hu_10{ 0.0 };
+		HuMoments() = delete;
+		HuMoments(double hu_1 = 0.0, double hu_2 = 0.0, double hu_3 = 0.0, double hu_4 = 0.0, double hu_5 = 0.0, double hu_6 = 0.0, double hu_7 = 0.0, double hu_8 = 0.0, double hu_9 = 0.0, double hu_10 = 0.0);
+		
+		static HuMoments calcHuMoments(const CentralMoments& normalizedCentralMoments);
 
-		HuMoments() = default;
-		HuMoments(double hu_1, double hu_2, double hu_3, double hu_4, double hu_5, double hu_6, double hu_7, double hu_8, double hu_9, double hu_10)
-			: hu_1{ hu_1 }, hu_2{ hu_2 }, hu_3{ hu_3 }, hu_4{ hu_4 }, hu_5{ hu_5 }, hu_6{ hu_6 }, hu_7{ hu_7 }, hu_8{ hu_8 }, hu_9{ hu_9 }, hu_10{ hu_10 } {};
+		bool isInRange(const HuMoments& min, const HuMoments& max) const;
+		double operator[](int index) const;
+
+	private:
+		std::vector<double> huValues;
 	};
-
-	HuMoments calcHuMoments(const CentralMoments& normalizedCentralMoments);
 }
--- src/HuMoments.cpp
+++ src/HuMoments.cpp
@@ -1,8 +1,36 @@
 #include "HuMoments.h"
 
+#include <algorithm>
+
 namespace POBR
 {
-	HuMoments calcHuMoments(const CentralMoments& normalizedCentralMoments)
+	HuMoments::HuMoments(double hu_1, double hu_2, double hu_3, double hu_4, double hu_5, double hu_6, double hu_7, double hu_8, double hu_9, double hu_10)
+		: huValues{ hu_1, hu_2, hu_3, hu_4, hu_5, hu_6, hu_7, hu_8, hu_9, hu_10 }
+	{
+	}
+
+	bool HuMoments::isInRange(const HuMoments& minHuMoments, const HuMoments& maxHuMoments) const
+	{
+		for (auto i = 0; i < huValues.size(); ++i)
+		{
+			auto value = huValues[i];
+			auto min = minHuMoments[i];
+			auto max = maxHuMoments[i];
+
+			if (value < min || value > max)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	double HuMoments::operator[](int index) const
+	{
+		return huValues[index];
+	}
+
+	HuMoments HuMoments::calcHuMoments(const CentralMoments& normalizedCentralMoments)
 	{
 		auto& nCM = normalizedCentralMoments;
 
@@ -19,7 +47,7 @@ namespace POBR
 		double hu_9 = nCM.M_20 * (nCM.M_21 * nCM.M_03 - std::pow(nCM.M_12, 2.0)) + nCM.M_02 * (nCM.M_30 * nCM.M_12 - std::pow(nCM.M_21, 2.0)) -
 			nCM.M_11 * (nCM.M_30 * nCM.M_03 - nCM.M_21 * nCM.M_12);
 		double hu_10 = std::pow((nCM.M_30 * nCM.M_03 - nCM.M_12 * nCM.M_21), 2.0) - 4.0 * (nCM.M_30 * nCM.M_12 - std::pow(nCM.M_21, 2.0)) * (nCM.M_03 * nCM.M_21 - nCM.M_12);
-		
+
 		auto huMoments = HuMoments{ hu_1, hu_2, hu_3, hu_4, hu_5, hu_6, hu_7, hu_8, hu_9, hu_10 };
 		return huMoments;
 	}
