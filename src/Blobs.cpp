8f372d99c89c02aceb48438c0fbc31ac3487ecd4
Maciej Adamski <madamskip1@gmail.com>
2023-01-13 18:11:59 +0100

    add Blobs::filterBlobsBySize, using erase-remove_if idiom

--- src/Blobs.cpp
+++ src/Blobs.cpp
@@ -1,13 +1,16 @@
 #include "Blobs.h"
+#include <algorithm>
 
 void POBR::Blobs::addBlob(const Blob& blob)
 {
 	blobs.push_back(blob);
 }
 
-void POBR::Blobs::filterBlobsBySize(const std::size_t size)
+void POBR::Blobs::filterBlobsBySize(const std::size_t& size)
 {
-	// TODO
+	blobs.erase(std::remove_if(blobs.begin(), blobs.end(), [&size](const auto& blob) {
+			return (blob.countPoints() < size);
+		}), blobs.end());
 }
 
 void POBR::Blobs::draw(cv::Mat& mat) const
